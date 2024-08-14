aaea91ac9ca6e348df813bac4e072561dd353879
n3vsk1y <alexafanasenko900@gmail.com>
2024-08-15 02:20:12 +0300

    db connect

--- main.py
+++ main.py
@@ -1,13 +1,47 @@
+from typing import List
+
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
-app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World!!!"}
+app = FastAPI(
+    title='Bing Journal'
+)
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}!"}
+users = [
+    {'id': 1, 'role': 'admin', 'name': 'Bob'},
+    {'id': 2, 'role': 'user', 'name': 'Garry'},
+    {'id': 3, 'role': 'moderator', 'name': 'Bill'},
+]
+
+trades = [
+    {'id': 1, 'user_id': 1, 'ticker': 'BTCUSDT', 'side': 'LONG', 'price': 60000, 'amount': 1}
+]
+
+
+class User(BaseModel):
+    id: int
+    role: str
+    name: str
+
+
+@app.get('/users/{user_id}', response_model=List[User])
+async def get_user(user_id: int):
+    return [user for user in users if user.get('id') == user_id]
+
+
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    ticker: str = Field(max_length=20)
+    side: str
+    price: float = Field(ge=0)
+    amount: float
+
+
+@app.post('/trades')
+async def add_trade(new_trades: List[Trade]):
+    trades.extend(new_trades)
+    return {'status': 200, 'data': trades}
+
+
