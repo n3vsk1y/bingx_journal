4a09942f73f9d0ed820f14334c35404c54634881
n3vsk1y <alexafanasenko900@gmail.com>
2024-08-16 01:11:15 +0300

    sqlalchemy 2.0 integration 2 built project structure

--- fastapi_backend/alembic/env.py
+++ fastapi_backend/alembic/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from fastapi_backend.config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS
-from fastapi_backend.models.models import metadata
+from fastapi_backend.sqlalchemy_database_orm.models import metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -38,7 +38,7 @@ target_metadata = metadata
 
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
+    """Run alembic in 'offline' mode.
 
     This configures the context with just a URL
     and not an Engine, though an Engine is acceptable
@@ -62,7 +62,7 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
+    """Run alembic in 'online' mode.
 
     In this scenario we need to create an Engine
     and associate a connection with the context.
---
+++ fastapi_backend/sqlalchemy_database_orm/models.py
@@ -0,0 +1,40 @@
+import datetime
+
+from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON
+from sqlalchemy.orm import Mapped, mapped_column
+
+from database import Base
+
+
+class TariffOrm(Base):
+    pass
+
+
+
+
+
+
+
+
+
+
+# metadata = MetaData()
+#
+# tariffs = Table(
+#     'tariffs',
+#     metadata,
+#     Column('id', Integer, primary_key=True),
+#     Column('plan', String, nullable=False),
+#     Column('permissions', JSON),
+# )
+#
+# users = Table(
+#     'users',
+#     metadata,
+#     Column('id', Integer, primary_key=True),
+#     Column('email', String, nullable=False),
+#     Column('username', String, nullable=False),
+#     Column('password', String, nullable=False),
+#     Column('registered_at', TIMESTAMP, default=datetime.datetime.now),
+#     Column('tariff', Integer, ForeignKey('tariffs.id')),
+# )
